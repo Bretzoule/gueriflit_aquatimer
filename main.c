eefa04ace480fc9a63e92fefd0456db97563f795
Hugo, François, Ghislain Pointereau <hugo.francois.ghislain.pointereau@eisti.fr>
2019-12-19 10:58:28 +0100

    ajout du premier main vide

---
+++ main.c
@@ -0,0 +1,13 @@
+/*!
+*  \file main.c
+*  \author Hugo POINTEREAU <pointereau@eisti.eu>
+*  \version 0.1
+*  \date 19/12/19
+*
+*  \brief jeu de la bataille navale
+*
+*/
+
+/* Inclusion des entêtes de librairies */
+#include <stdlib.h>
+#include <stdio.h>

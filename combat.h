1fbefcb2315c01aaee576e37c888b5d1b6991ea9
Thomas Lefloch <thomas.lefloch@eisti.fr>
2020-01-06 16:05:20 +0100

    Update combat.h

--- combat.h
+++ combat.h
@@ -3,15 +3,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-<<<<<<< HEAD
-
-=======
 #define ERREURALLOCATION -1
 #define BATEAU_VIVANT 1
 #define BATEAU_TOUCHE 2
 #define EAU_VIVANTE 0
 #define EAU_MORTE -1
->>>>>>> 4e8a6e31d55858f0cdca926e5a88246fa2d0d428
 #define N 10
 
 int effettir(int** ppint_grille, int int_coord_x, int int_coord_y);

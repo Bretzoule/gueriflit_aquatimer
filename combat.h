f23ec35053dbf9ed34a159988aa16bfbc8cf6e86
Hugo, François, Ghislain Pointereau <hugo.francois.ghislain.pointereau@eisti.fr>
2020-01-06 15:47:06 +0100

    Replace combat.h

--- combat.h
+++ combat.h
@@ -4,9 +4,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define ERREURALLOCATION -1
 #define N 10
 
-int tir(int** ppint_grille, int int_coord_x, int int_coord_y);
-
+int effettir(int** ppint_grille, int int_coord_x, int int_coord_y);
+int valitir(int** ppint_grille, int int_coord_x, int int_coord_y);
+void demandeCoord(char **coord);
+int tir(int** ppint_grille);
 #endif
